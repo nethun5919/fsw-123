43f681eebad6586bb3a1c4db1c27ad38632b5c28
Ursula.Netherland <ursula.netherland@bryantuniversity.edu>
2021-07-16 19:22:42 -0700

    add

---
+++ week-1/my-app/src/TodoList.js
@@ -0,0 +1,19 @@
+
+
+
+function TodoList () {
+
+    return (
+      <>
+        <h1>List of Todos</h1>
+        
+         <input type = "checkbox"/> <span>clean house</span>
+         <input type = "checkbox"/> <span>wash car</span>
+         <input type = "checkbox"/> <span>grocery shopping</span> 
+           
+          </>
+    );
+}
+   
+
+   export default TodoList;
