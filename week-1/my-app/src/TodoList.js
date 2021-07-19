1a742847fcde0d98ae377dbff469e327f0610ab7
nethun5919 <ursula.netherland@bryanuniversity.edu>
2021-07-19 13:34:32 -0700

    update
    adding updates to project

--- week-1/my-app/src/TodoList.js
+++ week-1/my-app/src/TodoList.js
@@ -8,8 +8,8 @@ function TodoList () {
         <h1>List of Todos</h1>
         
          <input type = "checkbox"/> <span>clean house</span>
-         <input type = "checkbox"/> <span>wash car</span>
-         <input type = "checkbox"/> <span>grocery shopping</span> 
+         <div><input type = "checkbox"/> <span>wash car</span></div>
+        <div> <input type = "checkbox"/> <span>grocery shopping</span> </div>
            
           </>
     );
